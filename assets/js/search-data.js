442c4962e1c176efbb09e2a7c56ea7b70e4e7c91
ChenchenKang <124611725+ChenchenKang@users.noreply.github.com>
2025-12-20 05:50:31 +0000

    Deploying to gh-pages from @ ChenchenKang/chenchenkang.github.io@2e9bb1fbbca5ca6d9436791e853ecc5e566ead23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-media",
+          title: "Media",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/media/";
+          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
