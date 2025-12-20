8fd0ce51de4104345f3cda21b532d962cba09844
ChenchenKang <124611725+ChenchenKang@users.noreply.github.com>
2025-12-20 05:56:19 +0000

    Deploying to gh-pages from @ ChenchenKang/chenchenkang.github.io@5487df0652fedc9588528bfb71788d187c99a247 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/media/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "post-a-post-with-plotly-js",
         
