0f462e04c47d426090ac1c8837ae3e9bc61a2e38
ChenchenKang <124611725+ChenchenKang@users.noreply.github.com>
2026-07-24 00:01:48 +0000

    Deploying to gh-pages from @ ChenchenKang/chenchenkang.github.io@f7f8e76c5c6c40e0b14e2d46a2782ac11e606f88 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,7 +45,7 @@ ninja.data = [{
             window.location.href = "/extension/";
           },
         },{id: "nav-news",
-          title: "news",
+          title: "News",
           description: "",
           section: "Navigation",
           handler: () => {
